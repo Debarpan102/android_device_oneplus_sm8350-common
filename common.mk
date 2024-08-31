f8af33de26469882ea49f8e2e1ccae5a32a06e3d
sx75 <neoroms69@gmail.com>
2024-08-31 13:31:46 +0530

    sm8350-common: Make alternative brightness algorithm for low gamma overrideable

--- common.mk
+++ common.mk
@@ -124,6 +124,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.sys.brightness.low.gamma?=true
+
+# Dex to make OTA go faster
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    dalvik.vm.boot-dex2oat-cpu-set=1,2,3,4,5,6,7 \
+    dalvik.vm.boot-dex2oat-threads=7 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-threads=8
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
