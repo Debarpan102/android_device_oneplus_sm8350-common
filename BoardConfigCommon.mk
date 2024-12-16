c3523d8366d137c004f8c4d063f2d541cfaed875
Debarpan102 <91316829+Debarpan102@users.noreply.github.com>
2024-12-16 17:43:02 +0530

    Reverted Openssl commit

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,11 +111,11 @@ BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8350
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
 TARGET_KERNEL_NO_GCC := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    HOSTCFLAGS="-Wno-unused-command-line-argument"
+#TARGET_KERNEL_ADDITIONAL_FLAGS := \
+#   HOSTCFLAGS="-Wno-unused-command-line-argument"
 
 # Include openssl headers
-CFLAGS += -I/usr/include/openssl
+#CFLAGS += -I/usr/include/openssl
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
