1e4f03cd73da32d1dd962eae3610c2c1f317b337
Debarpan102 <debarpan102github@gmail.com>
2025-07-17 01:20:22 +0530

    Revert "sm8350-common: Switch to NCM USB tethering"
    *Breaks USB Tethering on ice
    
    This reverts commit 9bc60707e5fdb9a395410893fd99be827da03fa6.

--- common.mk
+++ common.mk
@@ -223,7 +223,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
-    NcmTetheringOverlay \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
